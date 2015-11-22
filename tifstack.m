7346c67917502dbb26bab8725579296ef109f049
kclary <kclary@cs.umass.edu>
2015-11-22 13:39:42 -0500

    added comments

--- tifstack.m
+++ tifstack.m
@@ -1,4 +1,6 @@
 function vol = tifstack(tifPath)
+% TIFSTACK takes a directory of tif images and combines them in an image stack
+% TIFPATH is the path of the directory containing tif images
 
 list = dir(tifPath);
 list = list(3:end); %trim '.' and '..'
@@ -6,7 +8,7 @@ list = list(3:end); %trim '.' and '..'
 %get image dimensions by loading first image
 slice = imread(fullfile(tifPath, list(1).name));
 
-n = numel(list);
+n = numel(list); % number of images in directory
 X = size(slice, 1);
 Y = size(slice, 2);
 vol = zeros(X,Y,n);
