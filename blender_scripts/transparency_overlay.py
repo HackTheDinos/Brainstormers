bcf0f32539ee6b9b50b2b618944b69be833c6a67
laceylaborde <laceylaborde@gmail.com>
2015-11-22 11:45:38 -0500

    Lacey/Shawn - Add object color generator

--- blender_scripts/transparency_overlay.py
+++ blender_scripts/transparency_overlay.py
@@ -1,6 +1,10 @@
 import bpy
+import random
+from random import randint
+
 scene = bpy.context.scene
 initial_transparency = .3
+object_count = len(scene.objects)
 transparency_decrease = initial_transparency/len(scene.objects)
 
 def sort_by_volume(objects):
@@ -9,6 +13,15 @@ def sort_by_volume(objects):
 		volume = obj.dimensions[0] * obj.dimensions[1] * obj.dimensions[2]
 		volumes[obj] = volume
 	return sorted(volumes.items(), key=lambda x:x[1])
+
+def set_color(obj):
+	obj.active_material.diffuse_color = get_color()
+
+def get_color():
+	color1 = randint(0, 10) / 10
+	color2 = randint(0, 10) / 10
+	color3 = randint(0, 10) / 10
+	return (color1, color2, color3)
 
 def modify_objects(sortedVolumeTuples):
 	transparency = initial_transparency
@@ -19,6 +32,7 @@ def modify_objects(sortedVolumeTuples):
 				add_material(current_object)
 			set_transparency(current_object, transparency);
 			orient(current_object)
+			set_color(current_object)
 			transparency -= transparency_decrease
 
 def is_not_lamp_or_camera(obj):
@@ -58,7 +72,6 @@ def create_lamp():
 	lamp_object = bpy.data.object.new(name="New Lamp", object_data=lamp_block)
 	scene.object.link(lamp_object)
 	return lamp_object
-
 
 def run():
 	center_lamp()
